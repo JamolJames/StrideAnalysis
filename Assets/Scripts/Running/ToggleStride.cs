04dcc3073e0f81fb66aaa5dfb6b4a3a1a68fa5ea
Jamol James <jamolbjames@gmail.com>
2024-11-01 09:48:05 -0700

    Add new menu with interaction

---
+++ Assets/Scripts/Running/ToggleStride.cs
@@ -0,0 +1,19 @@
+using Meta.XR.ImmersiveDebugger.UserInterface.Generic;
+using UnityEngine;
+
+public class ToggleStride : MonoBehaviour
+{
+    public Toggle toggle;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
